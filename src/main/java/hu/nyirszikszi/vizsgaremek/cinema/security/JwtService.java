a6d316ccd7870a2ceae29aee40abe4351621d362
Kaszners <21d_kaszners@nyirszikszi.hu>
2026-02-12 00:09:54 +0100

    Add JwtService

---
+++ src/main/java/hu/nyirszikszi/vizsgaremek/cinema/security/JwtService.java
@@ -0,0 +1,33 @@
+package hu.nyirszikszi.vizsgaremek.cinema.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+
+
+    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+
+    public String generateToken(String username, String role){
+
+        return Jwts.builder()
+                .subject(username)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(
+                        new Date(System.currentTimeMillis()+1000*60*60)
+                )
+                .signWith(secretKey)
+                .compact();
+    }
+
+
+}
